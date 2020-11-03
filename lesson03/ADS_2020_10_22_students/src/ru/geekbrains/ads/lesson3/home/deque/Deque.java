0c4a68fa41aab89561d186ebbd1670216517ca2b
egorbarinov <egor.barinov@gmail.com>
2020-11-03 11:26:22 +0300

    added code from homework to the lesson03

---
+++ lesson03/ADS_2020_10_22_students/src/ru/geekbrains/ads/lesson3/home/deque/Deque.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.ads.lesson3.home.deque;
+
+import ru.geekbrains.ads.lesson3.myqueue.Queue;
+
+public interface Deque<E> extends Queue<E> {
+
+    boolean insertLeft(E value);
+    boolean insertRight(E value);
+    E removeLeft();
+    E removeRight();
+    E peekLast();
+}
