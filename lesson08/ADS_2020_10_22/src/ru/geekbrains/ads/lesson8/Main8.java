1ce22aa29d80824c296a4634ecaaa83689f7c3b2
egorbarinov <59361760+egorbarinov@users.noreply.github.com>
2021-02-01 20:00:12 +0300

    added code from homework to the lesson08 (#8)

--- lesson08/ADS_2020_10_22/src/ru/geekbrains/ads/lesson8/Main8.java
+++ lesson08/ADS_2020_10_22/src/ru/geekbrains/ads/lesson8/Main8.java
@@ -4,14 +4,14 @@ public class Main8 {
 
     public static void main(String[] args) {
 //        HashTable<Product, Integer> hashTable = new HashTableImpl<>(5);//5 * 2 = 10
-//        HashTable<Product, Integer> hashTable = new LinkedHashTableImpl<>(5);//5 * 2 = 10
-        HashTable<Product, Integer> hashTable = new DoubleHashTableImpl<>(5);//5 * 2 = 10
+        HashTable<Product, Integer> hashTable = new LinkedHashTableImpl<>(5);//5 * 2 = 10
+//        HashTable<Product, Integer> hashTable = new DoubleHashTableImpl<>(5);//5 * 2 = 10
 
         hashTable.put(new Product(1, "Orange"), 150);
         hashTable.put(new Product(77, "Banana"), 100);
         hashTable.put(new Product(77, "Carrot"), 228);
         hashTable.put(new Product(60, "Lemon"), 250);
-//        hashTable.put(new Product(52, "Milk"), 120);
+        hashTable.put(new Product(52, "Milk"), 120);
         hashTable.put(new Product(21, "Potato"), 67);
 
         System.out.println("Size is " + hashTable.size());
